997aea9db44929fed258b80a5537ca9efeabf4da
princeraman <olagunjuraman@gmail.com>
2020-08-04 17:28:37 +0100

    try again

--- server.js
+++ server.js
@@ -3,11 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=>{
-<<<<<<< HEAD
     res.send('hello-world, welcome to the complete website, i dey master branch')
-=======
-    res.send('hello-world, welcome to the complete website, i deleted the deploy key i am in the dev branches')
->>>>>>> 190f821ed24fdc5ba501f8212e8ea1cc6f3d2447
 });
 
 
